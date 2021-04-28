d20e1bf535aabe807ea21b287e1489d07c12ec55
CathrineChernogor <chernogor_k@mail.ru>
2021-04-28 11:37:42 +0800

    add photo capturing

---
+++ app/src/main/java/com/example/mahjongapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.mahjongapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button takePicture;
+    ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        takePicture = findViewById(R.id.takePicture);
+        imageView = findViewById(R.id.image);
+
+        dispatchTakePictureIntent();
+
+
+//        todo: count score
+//        todo: draw ui
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            Log.e("error", e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+
+            predictClasses(imageBitmap);
+
+
+        }
+    }
+
+    private void predictClasses(Bitmap image) {
+//        todo: predict classes
+    }
+}
---
+++ app/src/main/java/com/example/mahjongapp/ModelClass.java
@@ -0,0 +1,8 @@
+package com.example.mahjongapp;
+
+public class ModelClass {
+    public static String[] MODEL_CLASSES = new String[]{
+            "classes names",
+    };
+    // further add via json
+}
